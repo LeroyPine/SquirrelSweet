fc061d0da9103e3cc1babfb7735b4fb154c4c101
luobs <luobs@ziroom.com>
2021-08-29 22:08:58 +0800

    feature: self-write

---
+++ src/main/java/com/squirrel/java/lock/LockT.java
@@ -0,0 +1,19 @@
+package com.squirrel.java.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class LockT {
+
+    public static ReentrantLock lock = new ReentrantLock();
+
+    public static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public static void main(String[] args) {
+        lock.lock();
+        Lock lock = readWriteLock.readLock();
+        lock.lock();
+    }
+}
