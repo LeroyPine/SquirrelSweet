52f0a2b8292e55ffa73e021ac80f481e11ee1c43
luobs <luobs@ziroom.com>
2021-09-11 10:00:15 +0800

    feature: arth

---
+++ src/main/java/com/squirrel/java/leet/树/面试/二叉树的前序遍历.java
@@ -0,0 +1,37 @@
+package com.squirrel.java.leet.树.面试;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 二叉树的前序遍历 {
+
+
+    /**
+     * 二叉树的前序 ：：     根左右
+     *
+     * @param root
+     * @return
+     */
+
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        preOrder(list, root);
+
+        return list;
+    }
+
+    private void preOrder(List<Integer> list, TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        preOrder(list,root.left);
+        preOrder(list, root.right);
+
+    }
+
+
+}
