31804c8418fb427bc9bbf46406a9f6ddfe00e939
luobs <luobs@ziroom.com>
2021-09-22 20:40:27 +0800

    feature: arth  冲！！！！

--- src/main/java/com/squirrel/java/leet/数组/两数之和.java
+++ src/main/java/com/squirrel/java/leet/数组/两数之和.java
@@ -30,19 +30,19 @@ public class 两数之和 {
      */
     public int[] twoSum(int[] nums, int target) {
 
-        //
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
 
             if (map.containsKey(target - nums[i])) {
-                return new int[]{map.get(target - nums[i]), i};
+                return new int[]{map.get(target - nums[i]), i}
+            } else {
+                map.put(nums[i], i);
             }
-            map.put(nums[i], i);
 
         }
 
-        return null;
+        return new int[]{0};
     }
 
 }
