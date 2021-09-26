f72fd1b3eeb605e906fdd059f5c866cc3c49f2fe
luobs <luobs@ziroom.com>
2021-09-26 18:24:11 +0800

    feature: arth  冲！！！！

---
+++ src/main/java/com/squirrel/java/leet/链表/meet/缺失的第一个正数.java
@@ -0,0 +1,28 @@
+package com.squirrel.java.leet.链表.meet;
+
+/**
+ * <p></p>
+ * <p>
+ * <PRE>
+ * <BR>    修改记录
+ * <BR>-----------------------------------------------
+ * <BR>    修改日期         修改人          修改内容
+ * </PRE>
+ *
+ * @author luobs
+ * @version 1.0
+ * @date Created in 2021年09月26日 17:59
+ * @since 1.0
+ */
+public class 缺失的第一个正数 {
+/*
+    public int firstMissingPositive(int[] nums) {
+
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+    }*/
+}
