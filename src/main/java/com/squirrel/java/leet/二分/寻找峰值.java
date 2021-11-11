4f15b3973a922c108d761a3a3d5eb2005aab370e
罗宝松 <paul_pine@163.com>
2021-11-11 20:58:00 +0800

    feature:寻找峰值

--- src/main/java/com/squirrel/java/leet/二分/寻找峰值.java
+++ src/main/java/com/squirrel/java/leet/二分/寻找峰值.java
@@ -4,20 +4,42 @@ public class 寻找峰值 {
 
 
     public int findPeakElement(int[] nums) {
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left < right) {
-
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = -1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-
-            if (nums[mid] > nums[mid + 1]) {
-                right = mid;
+            if (compare(nums, mid, mid - 1) > 0 && compare(nums, mid, mid + 1) > 0) {
+                ans = mid;
+                break;
+            }
+            if (compare(nums, mid, mid - 1) > 0) {
+                left = mid + 1;
             } else {
-                left = mid - 1;
+                right = mid - 1;
             }
         }
-        return left;
+        return ans;
+
     }
+
+    public int[] getIndex(int[] nums, int index) {
+        if (index == -1 || index == nums.length) {
+            return new int[]{0, 0};
+        }
+        return new int[]{1, nums[index]};
+    }
+
+    // 然后书写compare方法
+    public int compare(int[] nums, int left, int right) {
+        int[] nums1 = getIndex(nums, left);
+        int[] nums2 = getIndex(nums, right);
+        if (nums1[0] != nums2[0]) {
+            return nums1[0] > nums2[0] ? 1 : -1;
+        }
+        if (nums1[1] == nums2[1]) {
+            return 0;
+        }
+        return nums1[1] > nums2[1] ? 1 : -1;
+    }
+
 }
