3cf569f59282c827b7ebb6da1be12b8bc2bf60e8
罗宝松 <paul_pine@163.com>
2021-12-18 16:50:38 +0800

    feature: arth

--- src/main/java/com/squirrel/java/leet/链表/meet/二叉树与双向链表.java
+++ src/main/java/com/squirrel/java/leet/链表/meet/二叉树与双向链表.java
@@ -1,19 +1,33 @@
 package com.squirrel.java.leet.链表.meet;
 
-public class 二叉搜索树与双向链表 {
+public class 二叉树与双向链表 {
 
 
-    Node pre = null, head = null;
-
+    /**
+     * 思路:
+     * 1. 递归终止条件:节点为null
+     * 2. 递归左子树,即dfs(cur.left)
+     * 3. 构件链表:
+     * 1.当pre为空时,代表正在访问链表头节点
+     * 2.当pre不为空的时候,修改双向节点引用。
+     * 3.
+     *
+     * @param root 根节点
+     * @return 链表
+     */
     public Node treeToDoublyList(Node root) {
         if (root == null) {
             return null;
         }
         dfs(root);
+        // 头尾节点互相指向
         head.left = pre;
         pre.right = head;
         return head;
     }
+
+    // 定义头尾节点
+    Node pre = null, head = null;
 
     /**
      * @param root
@@ -22,16 +36,16 @@ public class 二叉搜索树与双向链表 {
         if (root == null) {
             return;
         }
-        // 中序遍历
+        // 中序遍历最左侧,为根节点
         dfs(root.left);
         if (pre == null) {
-            // 标记为头结点
             head = root;
         } else {
+            // 链表组合
             pre.right = root;
         }
         root.left = pre;
-        // 然后去访问右节点
+        // pre一直移动
         pre = root;
         dfs(root.right);
     }
